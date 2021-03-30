1e6fafb71d8e1995281630dbfabc0fa0036c4fdb
takuchalle <me@takuchalle.dev>
2021-03-30 11:02:33 +0900

    add new file

---
+++ mod.ts
@@ -0,0 +1,12 @@
+import { serve } from "https://deno.land/x/sift@0.1.6/mod.ts";
+
+serve({
+  "/": () =>
+    new Response("デノ", {
+      headers: { "content-type": "text/html; charset=utf-8" },
+    }),
+  404: () =>
+    new Response("ありまへん", {
+      headers: { "content-type": "text/html; charset=utf-8" },
+    }),
+});
